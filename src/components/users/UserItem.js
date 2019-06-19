040577fdebce376c6d78a5dbf8a5b3eca6a6f04f
Rakesh Arunachalam <rakeshpetit@gmail.com>
2019-06-19 12:35:19 +0100

    Class based and functional components

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,34 +1,28 @@
-import React, { Component } from 'react'
+import React from 'react';
+import PropTypes from 'prop-types';
 
-export class UserItem extends Component {
-  constructor() {
-    super()
-    this.state = {
-      id: 'id',
-      login: 'Koala',
-      image_url: 'http://oi50.tinypic.com/kexbfq.jpg',
-      html_url: 'http://tinypic.com/view.php?pic=kexbfq&s=6&hid=0&tag=koala'
-    }
-  }
-  render() {
-    const { id, login, image_url, html_url } = this.state
-    return (
-      <div className="card text-center">
-        <img
-          className="round-img"
-          style={{ width: '60px', backgroundColor: 'red' }}
-          alt=""
-          src={image_url}
-        />
-        <h3>{login}</h3>
-        <div>
-          <a href={html_url} className="btn btn-dark btn-sm my-1">
-            More
-          </a>
-        </div>
+const UserItem = ({ user: { login, avatar_url, html_url } }) => {
+  return (
+    <div className='card text-center'>
+      <img
+        src={avatar_url}
+        alt=''
+        className='round-img'
+        style={{ width: '60px' }}
+      />
+      <h3>{login}</h3>
+
+      <div>
+        <a href={html_url} className='btn btn-dark btn-sm my-1'>
+          More
+        </a>
       </div>
-    )
-  }
-}
+    </div>
+  );
+};
 
-export default UserItem
+UserItem.propTypes = {
+  user: PropTypes.object.isRequired
+};
+
+export default UserItem;
