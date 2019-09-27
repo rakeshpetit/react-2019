c4acc4db3206ac456d5c5b3f81dafcd45f9140e4
rakeshpetit <rakeshpetit@gmail.com>
2019-09-27 11:45:09 +0100

    Refactor App js

--- src/App.js
+++ src/App.js
@@ -1,102 +1,87 @@
-import React, { Fragment, Component } from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Navbar from './components/layout/Navbar';
-import Users from './components/users/Users';
-import User from './components/users/User';
-import Search from './components/users/Search';
-import Alert from './components/layout/Alert';
-import About from './components/pages/About';
-import axios from 'axios';
-import './App.css';
+import React, { Fragment, useState } from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Navbar from './components/layout/Navbar'
+import Users from './components/users/Users'
+import User from './components/users/User'
+import Search from './components/users/Search'
+import Alert from './components/layout/Alert'
+import About from './components/pages/About'
+import axios from 'axios'
+import './App.css'
 
-class App extends Component {
-  state = {
-    users: [],
-    user: {},
-    loading: false,
-    alert: null
-  };
+const App = () => {
+  const [users, setUsers] = useState([])
+  const [user, setUser] = useState({})
+  const [loading, setLoading] = useState(false)
+  const [alert, setAlert] = useState(null)
 
   // Search Github users
-  searchUsers = async text => {
-    this.setState({ loading: true });
+  const searchUsers = async text => {
+    setLoading(true)
 
     const res = await axios.get(
-      `https://api.github.com/search/users?q=${text}&client_id=${
-        process.env.REACT_APP_GITHUB_CLIENT_ID
-      }&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
-    );
-
-    this.setState({ users: res.data.items, loading: false });
-  };
+      `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    )
+    setUsers(res.data.items)
+    setLoading(false)
+  }
 
   // Get single Github user
-  getUser = async username => {
-    this.setState({ loading: true });
-
+  const getUser = async username => {
+    setLoading(true)
     const res = await axios.get(
-      `https://api.github.com/users/${username}?client_id=${
-        process.env.REACT_APP_GITHUB_CLIENT_ID
-      }&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
-    );
+      `https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    )
 
-    this.setState({ user: res.data, loading: false });
-  };
+    setUser(res.data)
+    setLoading(false)
+  }
 
   // Clear users from state
-  clearUsers = () => this.setState({ users: [], loading: false });
+  const clearUsers = () => {
+    setUsers([])
+    setLoading(false)
+  }
 
   // Set Alert
-  setAlert = (msg, type) => {
-    this.setState({ alert: { msg, type } });
-
-    setTimeout(() => this.setState({ alert: null }), 5000);
-  };
-
-  render() {
-    const { users, user, loading } = this.state;
-
-    return (
-      <Router>
-        <div className='App'>
-          <Navbar />
-          <div className='container'>
-            <Alert alert={this.state.alert} />
-            <Switch>
-              <Route
-                exact
-                path='/'
-                render={props => (
-                  <Fragment>
-                    <Search
-                      searchUsers={this.searchUsers}
-                      clearUsers={this.clearUsers}
-                      showClear={users.length > 0 ? true : false}
-                      setAlert={this.setAlert}
-                    />
-                    <Users loading={loading} users={users} />
-                  </Fragment>
-                )}
-              />
-              <Route exact path='/about' component={About} />
-              <Route
-                exact
-                path='/user/:login'
-                render={props => (
-                  <User
-                    {...props}
-                    getUser={this.getUser}
-                    user={user}
-                    loading={loading}
-                  />
-                )}
-              />
-            </Switch>
-          </div>
-        </div>
-      </Router>
-    );
+  const saveAlert = (msg, type) => {
+    setAlert({ msg, type })
+    setTimeout(() => setAlert(null), 5000)
   }
+
+  return (
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="container">
+          <Alert alert={alert} />
+          <Switch>
+            <Route
+              exact
+              path="/"
+              render={props => (
+                <Fragment>
+                  <Search
+                    searchUsers={searchUsers}
+                    clearUsers={clearUsers}
+                    showClear={users.length > 0 ? true : false}
+                    setAlert={saveAlert}
+                  />
+                  <Users loading={loading} users={users} />
+                </Fragment>
+              )}
+            />
+            <Route exact path="/about" component={About} />
+            <Route
+              exact
+              path="/user/:login"
+              render={props => <User {...props} getUser={getUser} user={user} loading={loading} />}
+            />
+          </Switch>
+        </div>
+      </div>
+    </Router>
+  )
 }
 
-export default App;
+export default App
--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,43 +1,33 @@
-import React, { useState } from 'react';
-import PropTypes from 'prop-types';
+import React, { useState } from 'react'
+import PropTypes from 'prop-types'
 
-const Search = ({setAlert, searchUsers, showClear, clearUsers}) => {
+const Search = ({ setAlert, searchUsers, showClear, clearUsers }) => {
   const [text, setText] = useState('')
   const onSubmit = e => {
-    e.preventDefault();
+    e.preventDefault()
     if (text === '') {
-      setAlert('Please enter something', 'light');
+      setAlert('Please enter something', 'light')
     } else {
-      searchUsers(text);
-      setText('');
+      searchUsers(text)
+      setText('')
     }
-  };
+  }
 
-  const onChange = e => setText(e.target.value);
+  const onChange = e => setText(e.target.value)
 
   return (
     <div>
-      <form onSubmit={onSubmit} className='form'>
-        <input
-          type='text'
-          name='text'
-          placeholder='Search Users...'
-          value={text}
-          onChange={onChange}
-        />
-        <input
-          type='submit'
-          value='Search'
-          className='btn btn-dark btn-block'
-        />
+      <form onSubmit={onSubmit} className="form">
+        <input type="text" name="text" placeholder="Search Users..." value={text} onChange={onChange} />
+        <input type="submit" value="Search" className="btn btn-dark btn-block" />
       </form>
       {showClear && (
-        <button className='btn btn-light btn-block' onClick={clearUsers}>
+        <button className="btn btn-light btn-block" onClick={clearUsers}>
           Clear
         </button>
       )}
     </div>
-  );
+  )
 }
 
 Search.propTypes = {
@@ -45,6 +35,6 @@ Search.propTypes = {
   clearUsers: PropTypes.func.isRequired,
   showClear: PropTypes.bool.isRequired,
   setAlert: PropTypes.func.isRequired
-};
+}
 
-export default Search;
+export default Search
